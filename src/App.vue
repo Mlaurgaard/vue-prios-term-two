0e7c9c7b3e17ea4c5193113e4405ba2e7e64be53
Mlaurgaard <m.laurgaard@gmail.com>
2024-02-08 15:17:47 +0100

    Navbar close to complete
    App.vue
    -navbar updated with correct layout and links
    -sidebar is fully working with wanted layout
    -made comments for better overview

--- src/App.vue
+++ src/App.vue
@@ -8,94 +8,120 @@
         ref="drawerCheckbox"
       />
       <div class="drawer-content flex flex-col min-h-screen">
-        <!-- Navbar -->
-        <div class="w-full navbar shadow-md bg-base-100 h-[64px] z-10">
-          <div class="container md:px-4">
-            <div class="flex-none lg:hidden">
-              <label for="my-drawer-3" class="btn btn-square btn-ghost">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  class="inline-block w-6 h-6 stroke-current"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </label>
-            </div>
-            <div class="pl-0 pl-8">
+        <!-- NAVBAR -->
+        <div class="navbar shadow-md bg-base-100 h-[64px] z-10">
+          <!-- Burger Icon -->
+          <div class="flex-none lg:hidden">
+            <label
+              for="my-drawer-3"
+              class="rounded btn-ghost p-1 mr-2"
+              role="button"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                class="inline-block w-6 h-6 stroke-current mb-0.5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+            <!-- Navbar Title -->
+            <button class="py-2">
               <router-link to="/">
                 <p class="font-mono">GeneralStore</p>
               </router-link>
-            </div>
-            <!-- Sidebar Menu on smaller screens -->
-            <div class="flex-none hidden lg:block">
-              <ul class="menu menu-horizontal flex items-center font-bold">
-                <!-- Navbar menu content here -->
+            </button>
+          </div>
 
-                <li
-                  class="rounded"
-                  :class="{
-                    'focus:bg-primary bg-primary': $route.path === '/products',
-                  }"
-                >
-                  <router-link to="/products">Products</router-link>
-                </li>
-                <li
-                  class="rounded p-3"
-                  :class="{
-                    'focus:bg-primary bg-secondary': $route.path === '/about',
-                  }"
-                >
-                  <router-link to="/about">About</router-link>
-                </li>
-                <li
-                  class="rounded"
-                  :class="{
-                    'focus:bg-primary bg-secondary': $route.path === '/signup',
-                  }"
-                >
-                  <router-link to="/signup">Sign up</router-link>
-                </li>
-                <li
-                  class="rounded"
-                  :class="{
-                    'focus:bg-primary bg-secondary': $route.path === '/login',
-                  }"
-                >
-                  <router-link to="/login">Login</router-link>
-                </li>
-              </ul>
+          <!-- Navbar Buttons  -->
+          <div class="lg:flex hidden justify-between w-full">
+            <!-- Navbar Left -->
+            <div class="w-auto">
+              <!-- title / home -->
+              <button class="p-2 mx-2">
+                <router-link to="/">
+                  <p class="font-mono">GeneralStore</p>
+                </router-link>
+              </button>
+              <!-- products -->
+              <button
+                class="p-2 mx-1 rounded hover:bg-gray-200 active:bg-gray-300 font-bolder"
+              >
+                <router-link to="/products">
+                  <p class="p-1 px-2 text-sm font-bolder">Products</p>
+                </router-link>
+              </button>
+              <!-- about -->
+              <button
+                class="p-2 mx-1 rounded hover:bg-gray-200 active:bg-gray-300 font-bolder"
+              >
+                <router-link to="/about">
+                  <p class="p-1 px-2 text-sm font-bolder">About</p>
+                </router-link>
+              </button>
+            </div>
+            <!-- Navbar Right -->
+            <div class="rounded w-auto">
+              <!-- login -->
+              <button
+                class="p-2 mx-1 rounded hover:bg-gray-200 active:bg-gray-300 font-bolder"
+              >
+                <router-link to="/login">
+                  <p class="p-1 px-1 text-sm font-bolder">Login</p>
+                </router-link>
+              </button>
+              <!-- sign up -->
+              <button
+                class="p-2 mx-1 rounded hover:bg-gray-200 active:bg-gray-300 font-bolder"
+              >
+                <router-link to="/signup">
+                  <p class="p-1 px-1 text-sm font-bolder">Sign up</p>
+                </router-link>
+              </button>
             </div>
           </div>
         </div>
+        <!-- NAVBAR END -->
         <!-- Page content here -->
         <RouterView />
+        <!-- Footer -->
         <Footer />
       </div>
+      <!-- SIDEBAR -->
       <div class="drawer-side z-50">
         <label for="my-drawer-3" class="drawer-overlay"></label>
-        <ul class="menu py-4 w-80 min-h-full bg-base-300 font-bold">
-          <!-- Sidebar content here -->
+        <ul class="menu py-4 w-80 min-h-full bg-base-300 font-bolder">
+          <!-- Sidebar Title -->
+          <button class="py-2">
+            <router-link to="/">
+              <p class="font-mono">GeneralStore</p>
+            </router-link>
+          </button>
+          <!-- Sidebar buttons -->
           <li>
-            <router-link to="/" @click="handleClick()">Home</router-link>
-            <router-link to="/products" @click="handleClick()"
-              >Products</router-link
-            >
-            <router-link to="/about" @click="handleClick()">About</router-link>
+            <router-link class="my-1" to="/" @click="handleClick()"
+              >Home
+            </router-link>
+            <router-link class="my-1" to="/products" @click="handleClick()"
+              >Products
+            </router-link>
+            <router-link @click="handleClick()" class="my-1" to="/about"
+              >About
+            </router-link>
           </li>
           <li class="mb-0 mt-auto">
-            <router-link class="mb-0 mt-auto" to="/login" @click="handleClick()"
-              >Login</router-link
-            >
-            <router-link class="" to="/signup" @click="handleClick()"
-              >Sign up</router-link
-            >
+            <router-link class="my-1 mt-auto" to="/login" @click="handleClick()"
+              >Login
+            </router-link>
+            <router-link class="my-1" to="/signup" @click="handleClick()"
+              >Sign up
+            </router-link>
           </li>
         </ul>
       </div>
