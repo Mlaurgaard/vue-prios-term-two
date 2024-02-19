0c6ff9e280be6d227653dac65742b61f54db54fa
Mlaurgaard <m.laurgaard@gmail.com>
2024-02-19 16:53:45 +0100

    UserView update
    Fixed problem with user information card, only thing remaining is to setup login and local storage.
    Started working on purchase history and tabs.

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="flex w-100 bg-green-500">
+  <div class="flex w-100 bg-base-300">
     <!-- user card container phone screens -->
-    <div class="flex flex-col h-80 bg-gray-200 m-auto my-4 w-80 lg:hidden">
+    <div
+      class="flex flex-col h-80 bg-base-100 m-auto my-4 w-80 md:w-6/12 lg:w-9/12 lg:flex-row"
+    >
       <!-- user icon -->
-      <div class="mx-auto overflow-visible sticky">
+      <div class="mx-auto lg:my-auto lg:w-1/3 lg:flex lg:justify-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -17,81 +19,76 @@
           />
         </svg>
       </div>
-      <!-- profile name -->
-      <div class="mx-auto underline">USERNAME</div>
-      <!-- right side of card -->
-      <div class="flex flex-row">
-        <div class="flex flex-col w-1/2">
-          <!-- name -->
-          <span class="mt-2 mx-auto">Name</span>
-          <span class="mx-auto">Display name</span>
-          <!-- adress -->
-          <span class="mt-2 mx-auto">Adress</span>
-          <span class="mx-auto">Display Adress</span>
-        </div>
-        <!-- left side of card -->
-        <div class="flex flex-col w-1/2">
-          <!-- phone -->
-          <span class="mt-2 mx-auto">Phone</span>
-          <span class="mx-auto">Display Number</span>
-          <!-- Email -->
-          <span class="mt-2 mx-auto">Email</span>
-          <span class="mx-auto">Display Email</span>
-        </div>
-      </div>
-      <!-- edit button -->
-      <div class="mx-auto my-4">
-        <Button class="rounded bg-gray-400 p-1">Edit</Button>
-      </div>
-    </div>
-    <!-- Desktop -->
-    <!-- user card container desktop -->
-    <div class="flex flex-col h-80 bg-gray-200 m-auto my-4 w-96">
-      <!-- user icon -->
-      <div class="mx-auto overflow-visible sticky">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          class="w-20 h-20"
+      <div class="lg:w-2/3">
+        <div
+          class="flex mx-auto underline justify-center lg:flex lg:flex-row lg:h-1/5 lg:mt-4 lg:justify-center"
         >
-          <path
-            fill-rule="evenodd"
-            d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-            clip-rule="evenodd"
-          />
-        </svg>
+          USERNAME
+        </div>
+        <!-- space block -->
+        <div class="block my-6 lg:hidden"></div>
+        <!-- right/top side of card -->
+        <div class="lg:flex lg:flex-row lg:h-2/3">
+          <div class="flex flex-row lg:mx-auto">
+            <div class="flex flex-col w-1/2 lg:w-2/3 lg:flex-col">
+              <!-- name -->
+              <span class="mt-2 mx-auto lg:my-2">Name</span>
+              <span class="mx-auto">DisplayName</span>
+              <!-- space block -->
+              <div class="my-2 lg:my-4 lg:block"></div>
+              <!-- address -->
+              <span class="mt-2 mx-auto lg:my-2">Address</span>
+              <span class="mx-auto">DisplayAdress</span>
+            </div>
+            <div class="mx-24 hidden lg:block"></div>
+            <!-- left/bottom side of card -->
+            <div class="flex flex-col w-1/2 lg:w-2/3 lg:flex-col">
+              <!-- phone -->
+              <span class="mt-2 mx-auto lg:my-2">Phone</span>
+              <span class="mx-auto">DisplayNumber</span>
+              <!-- space block -->
+              <div class="my-2 lg:my-4 lg:block"></div>
+              <!-- Email -->
+              <span class="mt-2 mx-auto lg:my-2">Email</span>
+              <span class="mx-auto">DisplayEmail</span>
+            </div>
+          </div>
+          <!-- edit button -->
+        </div>
       </div>
       <!-- profile name -->
-      <div class="mx-auto underline">USERNAME</div>
-      <!-- right side of card -->
-      <div class="flex flex-row">
-        <div class="flex flex-col w-1/2">
-          <!-- name -->
-          <span class="mt-2 mx-auto">Name</span>
-          <span class="mx-auto">Display name</span>
-          <!-- adress -->
-          <span class="mt-2 mx-auto">Adress</span>
-          <span class="mx-auto">Display Adress</span>
-        </div>
-        <!-- left side of card -->
-        <div class="flex flex-col w-1/2">
-          <!-- phone -->
-          <span class="mt-2 mx-auto">Phone</span>
-          <span class="mx-auto">Display Number</span>
-          <!-- Email -->
-          <span class="mt-2 mx-auto">Email</span>
-          <span class="mx-auto">Display Email</span>
-        </div>
-      </div>
-      <!-- edit button -->
-      <div class="mx-auto my-4">
-        <Button class="rounded bg-gray-400 p-1">Edit</Button>
-      </div>
     </div>
   </div>
   <!-- purchases / history -->
-  <section>History</section>
+  <section>
+    <h2 class="flex justify-center my-2 font-bold">Purchase History</h2>
+    <div role="tablist" class="tabs tabs-lifted tabs-lg">
+      <a role="tab" class="tab tab-active">Recent</a>
+      <a role="tab" class="tab">History</a>
+    </div>
+    <div class="overflow-x-auto">
+      <table class="table md:table-md lg:table-lg">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Order Date</th>
+            <th>Order Id</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td></td>
+          <td>1</td>
+          <td>asihasihj</td>
+          <td>something</td>
+          <td>01.01.2024</td>
+          <td>19195</td>
+        </tbody>
+      </table>
+    </div>
+  </section>
 </template>
 
 <script setup>
