50d652b8b9cf35d0e158ad23f4431fc0b070d421
Mlaurgaard <m.laurgaard@gmail.com>
2024-03-19 10:38:57 +0100

    ProductsView Visuals
    Updated positioning of cards
    Changed how images in cards look to fit better.
    Applied a Buy button to the cards, not implemented buy function yet.
    Also added descriptive comments to document for future updates.

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -4,28 +4,37 @@
     <span>Loading Products...</span>
     <span class="loading loading-spinner text-neutral"></span>
   </div>
+  <!-- Card Positionals -->
   <div
-    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-10 mx-auto mt-2"
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-auto px-8 py-4 bg-base-300"
     v-else-if="!MyProductStore.isLoading"
   >
+    <!-- Card -->
     <div v-for="product in MyProductStore.myProducts" class="flex">
       <div
-        class="card w-96 bg-base-100 shadow-xl"
+        class="card w-96 bg-base-100 shadow-md"
         @click="getProductData(product)"
       >
-        <figure>
-          <img :src="product.image" alt="Shoes" height="50px" width="50px" />
+        <!-- Card Image -->
+        <figure class="h-14 w-24 p-6 mt-2 mx-auto">
+          <img :src="product.image" alt="Shoes" class="bg-cover" />
         </figure>
+        <!-- Card Body -->
         <div class="card-body">
-          <h2 class="card-title line-clamp-2">
+          <!-- Card Title -->
+          <h2 class="card-title line-clamp-2 text-sm">
             <router-link to="ProductCard">{{ product.title }}</router-link>
           </h2>
-          <p class="line-clamp-2">{{ product.description }}</p>
+          <!-- Card Description -->
+          <p class="line-clamp-2 text-xs">{{ product.description }}</p>
+          <!-- Buy button -->
+          <button class="rounded p-1 mt-2 ml-auto bg-base-300 text-xs">
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
   </div>
-  <pre><button @click="MyProductStore.sendSingleProductToState(product)"> click me</button></pre>
 </template>
 
 <script setup>
