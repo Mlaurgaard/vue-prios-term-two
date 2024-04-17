cf7c5738b12f3a838038e2e98502487859970f68
Mlaurgaard <m.laurgaard@gmail.com>
2024-04-17 14:45:34 +0200

    Aboutview
    layout corrections

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,13 @@
 <template>
-  <h3>USER LOGINS</h3>
-  <pre> user: 1 <br> Email "john@gmail.com" <br> Password: "m38rmF$" </pre>
-  <pre> user: 2 <br> Email "morrison@gmail.com" <br> Password: "83r5^_" </pre>
-  <pre> user: 3 <br> Email "kevin@gmail.com" <br> Password: "kev02937@" </pre>
-  <pre> user: 4 <br> Email "don@gmail.com" <br> Password: "ewedon" </pre>
-  <pre> user: 5 <br> Email "derek@gmail.com" <br> Password: "jklg*_56" </pre>
-  <pre> user: 6 <br> Email "david_r@gmail.com" <br> Password: "3478*#54" </pre>
-  <pre> user: 7 <br> Email "miriam@gmail.com" <br> Password: "f238&@*$" </pre>
-  <pre> user: 8 <br> Email "william@gmail.com" <br> Password: "William56$hj" </pre>
-  <pre> user: 9 <br> Email "kate@gmail.com" <br> Password: "kfejk@*_" </pre>
-  <pre> user: 10 <br> Email "jimmie@gmail.com" <br> Password: "klein*#%*" </pre>
+  <h3>LIST OF USER LOGINS</h3>
+  <pre> <span class="font-bold">User: 1</span>  <br> Email "john@gmail.com" <br> Password: "m38rmF$" </pre>
+  <pre> <span class="font-bold">User: 2</span> <br> Email "morrison@gmail.com" <br> Password: "83r5^_" </pre>
+  <pre> <span class="font-bold">User: 3</span><br> Email "kevin@gmail.com" <br> Password: "kev02937@" </pre>
+  <pre> <span class="font-bold">User: 4</span><br> Email "don@gmail.com" <br> Password: "ewedon" </pre>
+  <pre> <span class="font-bold">User: 5</span><br> Email "derek@gmail.com" <br> Password: "jklg*_56" </pre>
+  <pre> <span class="font-bold">User: 6</span><br> Email "david_r@gmail.com" <br> Password: "3478*#54" </pre>
+  <pre> <span class="font-bold">User: 7</span><br> Email "miriam@gmail.com" <br> Password: "f238&@*$" </pre>
+  <pre> <span class="font-bold">User: 8</span><br> Email "william@gmail.com" <br> Password: "William56$hj" </pre>
+  <pre> <span class="font-bold">User: 9</span><br> Email "kate@gmail.com" <br> Password: "kfejk@*_" </pre>
+  <pre> <span class="font-bold">User: 10</span><br> Email "jimmie@gmail.com" <br> Password: "klein*#%*" </pre>
 </template>
