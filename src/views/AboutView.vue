82497e7bf072d535813de81dcfb0814256d0a8c4
Mlaurgaard <m.laurgaard@gmail.com>
2024-04-18 12:14:00 +0200

    AboutView.vue
    Finished introduction part of page.
    Began working on "Tools" part before lunch.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,97 @@
 <template>
-  <h3>LIST OF USER LOGINS</h3>
-  <pre> <span class="font-bold">User: 1</span>  <br> Email "john@gmail.com" <br> Password: "m38rmF$" </pre>
-  <pre> <span class="font-bold">User: 2</span> <br> Email "morrison@gmail.com" <br> Password: "83r5^_" </pre>
-  <pre> <span class="font-bold">User: 3</span><br> Email "kevin@gmail.com" <br> Password: "kev02937@" </pre>
-  <pre> <span class="font-bold">User: 4</span><br> Email "don@gmail.com" <br> Password: "ewedon" </pre>
-  <pre> <span class="font-bold">User: 5</span><br> Email "derek@gmail.com" <br> Password: "jklg*_56" </pre>
-  <pre> <span class="font-bold">User: 6</span><br> Email "david_r@gmail.com" <br> Password: "3478*#54" </pre>
-  <pre> <span class="font-bold">User: 7</span><br> Email "miriam@gmail.com" <br> Password: "f238&@*$" </pre>
-  <pre> <span class="font-bold">User: 8</span><br> Email "william@gmail.com" <br> Password: "William56$hj" </pre>
-  <pre> <span class="font-bold">User: 9</span><br> Email "kate@gmail.com" <br> Password: "kfejk@*_" </pre>
-  <pre> <span class="font-bold">User: 10</span><br> Email "jimmie@gmail.com" <br> Password: "klein*#%*" </pre>
+  <h1 class="mx-auto my-4 font-bold text-lg">ABOUT</h1>
+  <div class="flex flex-col w-full bg-base-300">
+    <div class="ml-4 md:mx-auto font-bold underline"><p>Introduction</p></div>
+    <div class="ml-4 md:mx-auto text-sm md:text-center font-bolder">
+      <p class="my-2">Welcome to my very first Vue project!</p>
+      <p>
+        This is a project developed during my "apprenticeship" for web
+        development at Prios Kompetanse AS.
+      </p>
+      <p class="my-2 mb-4">
+        I decided to try simulating a webstore, which i thought would give me a
+        lot of variety and different types of challenges based on the list of
+        tasks assigned to me by my mentor. This decision turned out to be more
+        challenging than i would have anticipated, but ended up being both
+        rewarding and fun!
+      </p>
+      <h2 class="font-bold text-sm">Previous Experience:</h2>
+      <p class="my-2">
+        My experience with web development prior to this project was a
+        introduction class back in 2018, where we learned the basics of HTML,
+        CSS and Javascript.
+      </p>
+      <p class="my-2">
+        When i started my apprenticeship at Prios i began re-learning HTML & CSS
+        pretty much from scratch, from the start of october through november,
+        and Javascript throughout december (2023).
+      </p>
+      <p class="my-2">
+        My intoduction to framework development started in january 2024, where i
+        began learning Vue3 composition API, and started this project at the
+        beginning of february.
+      </p>
+      <p class="my-2">
+        That's enough about me, let me introduce you to my project starting with
+        Tools!
+      </p>
+    </div>
+    <div class="flex flex-col w-full bg-base-200">
+      <h1 class="ml-4 my-2 md:mx-auto font-bold underline">Tools:</h1>
+      <p class="ml-4 my-2">
+        The develpoment tools used during this project is:
+      </p>
+      <li class="ml-8">
+        <a class="btn-link underline" href="https://vuejs.org/" target="_blank"
+          >Vue3 Composition API
+        </a>
+      </li>
+      <li class="ml-8">
+        <a
+          class="btn-link underline"
+          href="https://pinia.vuejs.org/"
+          target="_blank"
+          >Pinia Store
+        </a>
+      </li>
+      <li class="ml-8">
+        <a
+          class="btn-link underline"
+          href="https://axios-http.com/"
+          target="_blank"
+          >Axios
+        </a>
+      </li>
+      <li class="ml-8">
+        <a
+          class="btn-link underline"
+          href="https://tailwindcss.com/"
+          target="_blank"
+          >Tailwindcss
+        </a>
+      </li>
+      <li class="ml-8">
+        <a
+          class="btn-link underline"
+          href="https://daisyui.com/"
+          target="_blank"
+          >Daisyui
+        </a>
+      </li>
+    </div>
+  </div>
+
+  <div class="hidden">
+    <h3>LIST OF USER LOGINS</h3>
+    <pre> <span class="font-bold">User: 1</span>  <br> Email "john@gmail.com" <br> Password: "m38rmF$" </pre>
+    <pre> <span class="font-bold">User: 2</span> <br> Email "morrison@gmail.com" <br> Password: "83r5^_" </pre>
+    <pre> <span class="font-bold">User: 3</span><br> Email "kevin@gmail.com" <br> Password: "kev02937@" </pre>
+    <pre> <span class="font-bold">User: 4</span><br> Email "don@gmail.com" <br> Password: "ewedon" </pre>
+    <pre> <span class="font-bold">User: 5</span><br> Email "derek@gmail.com" <br> Password: "jklg*_56" </pre>
+    <pre> <span class="font-bold">User: 6</span><br> Email "david_r@gmail.com" <br> Password: "3478*#54" </pre>
+    <pre> <span class="font-bold">User: 7</span><br> Email "miriam@gmail.com" <br> Password: "f238&@*$" </pre>
+    <pre> <span class="font-bold">User: 8</span><br> Email "william@gmail.com" <br> Password: "William56$hj" </pre>
+    <pre> <span class="font-bold">User: 9</span><br> Email "kate@gmail.com" <br> Password: "kfejk@*_" </pre>
+    <pre> <span class="font-bold">User: 10</span><br> Email "jimmie@gmail.com" <br> Password: "klein*#%*" </pre>
+  </div>
 </template>
