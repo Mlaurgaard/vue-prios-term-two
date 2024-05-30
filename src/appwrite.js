93eb8404aa07d46c88b942e28e540760e2120377
Mlaurgaard <m.laurgaard@gmail.com>
2024-05-30 10:53:48 +0200

    appwrite remove

--- src/appwrite.js
+++
@@ -1,9 +0,0 @@
-import { Client, Databases, Account } from "appwrite";
-
-const client = new Client();
-client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("6658352a003ae8257dda"); // Replace with your project ID
-
-export const account = new Account(client);
-export const databases = new Databases(client);
